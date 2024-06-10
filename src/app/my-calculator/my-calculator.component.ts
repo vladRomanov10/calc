f3332cb7ae51c9f8f1373686774e72a2a5ac223e
Vlad Romanov <vladromeur@gmail.com>
2024-06-10 14:46:37 +0500

    СВнедрени стройÐгой типизации Ð

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
 
+interface CalcGroup {
+  first: CalcVar
+  second: CalcVar
+  operation: CalcOperations
+}
+
+interface CalcVar {
+  value: number
+  modificator: CalcModifiers
+}
+
+enum CalcOperations {
+  plus = '+',
+  minus = '-',
+  multiply = '*',
+  divide = '/'
+}
+
+enum CalcModifiers {
+  none = 'none',
+  sin = 'sin',
+  cas = 'cas',
+  square = 'square'
+}
+
 @Component({
   selector: 'app-my-calculator',
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.css'
 })
 export class MyCalculatorComponent {
+  public calcOperations = CalcOperations
 
-  public first: number = 1;
-  public second: number = 1;
-
-  public operation: string = '+'
-
-  public operations: string[] = ['+', '-', '*', '/']
+  public calcModifiers = CalcModifiers
 
   public result?: number
 
